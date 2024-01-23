8e1fab79c6dea6ffa017a90c5c2cdaae00c2e326
dm-mrtnvch <dmvshn@gmail.com>
2024-01-24 01:41:27 +0400

    feat: list & read

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
-const list = async () => {
-    // Write your code here 
-};
+import path, {dirname} from 'path'
+import {fileURLToPath} from 'url'
+import fs from 'fs'
 
-await list();
+const list = async () => {
+  const basePath = dirname(fileURLToPath(import.meta.url))
+  const sourcePath = path.join(basePath, 'files')
+
+  try {
+    await fs.promises.access(sourcePath)
+  } catch (e) {
+    throw new Error('FS operation failed')
+  }
+
+  const files = await fs.promises.readdir(sourcePath)
+
+  for (const file of files) {
+    console.log(file)
+  }
+}
+
+await list()
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
-const read = async () => {
-    // Write your code here 
-};
+import path, {dirname} from 'path'
+import {fileURLToPath} from 'url'
+import fs from 'fs'
 
-await read();
+const read = async () => {
+  const basePath = dirname(fileURLToPath(import.meta.url))
+  const fileToReadPath = path.join(basePath, 'files/fileToRead.txt')
+
+  try {
+    await fs.promises.access(fileToReadPath)
+  } catch (e) {
+    throw new Error('FS operation failed')
+  }
+
+  const fileContent = await fs.promises.readFile(fileToReadPath, 'utf8')
+  console.log(fileContent)
+}
+
+await read()
