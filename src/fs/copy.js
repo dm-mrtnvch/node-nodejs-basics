5ad83bb30861711919086220b144b0fe86d7c21f
dm-mrtnvch <dmvshn@gmail.com>
2024-01-24 00:22:09 +0400

    feat: create & copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
+import fs from 'fs'
+import path, {dirname} from 'path'
+import {fileURLToPath} from 'url'
+
 const copy = async () => {
-    // Write your code here 
+  const basePath = dirname(fileURLToPath(import.meta.url))
+  const sourcePath = path.join(basePath, '/files')
+  const destinationPath = path.join(basePath, '/files_copy')
+
+  const isSourceFolderExist = fs.existsSync(sourcePath)
+  if (!isSourceFolderExist) throw new Error('FS operation failed')
+
+  const isDestinationFolderExist = fs.existsSync(destinationPath)
+  if (isDestinationFolderExist) throw new Error('FS operation failed')
+
+  const mkdirResult = await fs.promises.mkdir(destinationPath)
+  if (mkdirResult?.code === 'EEXIST') throw new Error('FS operation failed')
+
+  const files = await fs.promises.readdir(sourcePath)
+
+  if (files) {
+    for (const file of files) {
+      const sourceFile = path.join(sourcePath, file)
+      const destinationFile = path.join(destinationPath, file)
+      await fs.promises.copyFile(sourceFile, destinationFile)
+    }
+  }
 };
 
-await copy();
+await copy()
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
-const create = async () => {
-    // Write your code here 
-};
+import path, {dirname} from 'path'
+import {fileURLToPath} from 'url'
+import fs from 'fs'
 
-await create();
+const create = async () => {
+  const fileContent = 'I am fresh and young'
+  const __dirname = dirname(fileURLToPath(import.meta.url))
+  const filePath = path.join(__dirname, '/files/fresh.txt')
+
+  try {
+    await fs.promises.writeFile(filePath, fileContent, {flag: 'wx'})
+  } catch (e) {
+    throw new Error('FS operation failed')
+  }
+}
+
+await create()
