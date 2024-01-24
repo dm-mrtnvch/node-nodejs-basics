70f0cedcdd5f9720e2a84c15ad8121361eb6614c
dm-mrtnvch <dmvshn@gmail.com>
2024-01-25 02:11:58 +0400

    feat: ECMAScript & calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import fs from 'fs'
+import crypto from 'crypto'
+import path, {dirname} from 'path'
+import {fileURLToPath} from 'url'
 
-await calculateHash();
+const calculateHash = async () => {
+  const scriptFilePath = fileURLToPath(import.meta.url)
+  const __dirname = dirname(scriptFilePath)
+  const readFilePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt')
+
+  const fileStream = fs.createReadStream(readFilePath)
+  const hash = crypto.createHash('sha256')
+
+  fileStream.pipe(hash).setEncoding('hex')
+
+  hash.on('finish', () => {
+    console.log(hash.read())
+  })
+}
+
+await calculateHash()
